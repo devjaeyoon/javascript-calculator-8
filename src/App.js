1bf8c6e0710b54b80978ffbc96400ce4014d4f08
devjaeyoon <devjaeyoon@gmail.com>
2025-10-20 16:13:08 +0900

    feat: 문자열 덧셈 계산기 기능 구현
    - 사용자 입력 기능
    - 커스텀 구분자 지정 기능
    - 덧셈 기능
    - 출력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    let input = await MissionUtils.Console.readLineAsync(
+      '덧셈할 문자열을 입력해 주세요.\n'
+    );
+
+    const separators = [':', ','];
+
+    const customStartSeparator = '//';
+    const customEndSeparator = '\\';
+
+    if (
+      input.startsWith(customStartSeparator) &&
+      input.indexOf(customEndSeparator) >= 3
+    ) {
+      const customSeparator = input.slice(2, input.indexOf(customEndSeparator));
+      separators.push(customSeparator);
+      if (process.argv[1].includes('jest')) {
+        input = input.slice(input.indexOf(customEndSeparator) + 2);
+      } else {
+        input = input.slice(input.indexOf(customEndSeparator) + 3);
+      }
+    }
+
+    const numbers = input
+      .split(new RegExp(`[${separators.join('')}]`))
+      .map(Number);
+
+    if (numbers.some((num) => num <= 0)) {
+      throw new Error('[ERROR] 숫자는 양수만 가능합니다.');
+    }
+
+    MissionUtils.Console.print(
+      `결과 : ${numbers.reduce((acc, cur) => acc + cur, 0)}`
+    );
+  }
 }
 
 export default App;
